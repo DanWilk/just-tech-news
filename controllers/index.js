86551875e5f6845f83feecb968dde0f58befa4d6
DanWilk <dcwilkinson@wilktech.com>
2021-12-04 20:16:14 -0500

    added login/out functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 
 // adds the /api inside of the route
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // if request is not to an implemented endpoint
 router.use((req, res) => {
